1e05550167f0c1bc4804f9324e3e1b77d9eff35d
YIyi-linlin <geniesuqiu@gmail.com>
2019-04-02 20:48:31 +0800

    regex

--- test.py
+++ test.py
@@ -4,6 +4,19 @@
 # @Author  : Yiyilinlin
 # @Software: PyCharm
 
-#test
-for i in range(1, 20):
-    print(i)
+# regex-exercise
+
+import re
+
+
+def is_valid_email(addr):
+    if re.match(r'[0-9a-zA-Z\_\.]+@[0-9a-zA-Z\_]+\.com', addr):
+        return True
+
+
+# 测试
+assert is_valid_email('someone@gmail.com')
+assert is_valid_email('bill.gates@microsoft.com')
+assert not is_valid_email('bob#example.com')
+assert not is_valid_email('mr-bob@example.com')
+print('ok')
